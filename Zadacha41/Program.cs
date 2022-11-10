d4f456f4a2af15c04a3daace03d8d447ba6d1989
Marat Yun <tigerplyus@mail.ru>
2022-11-11 00:53:52 +0300

    Решение задачи 43

--- Zadacha41/Program.cs
+++ Zadacha41/Program.cs
@@ -5,21 +5,22 @@
 Console.WriteLine($"Сколько М чисел хотите ввести?");
 int m = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[m];
-void InputNumbers(int m){
-for (int i = 0; i < m; i++)
-  {
-    Console.Write($"Введи {i+1} число: ");
-    numbers[i] = Convert.ToInt32(Console.ReadLine());
-  }
+void InputNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
 }
 int BigNumNol(int[] numbers)
 {
-  int count = 0;
-  for (int i = 0; i < numbers.Length; i++)
-  {
-    if(numbers[i] > 0 ) count += 1; 
-  }
-  return count;
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) count += 1;
+    }
+    return count;
 }
 InputNumbers(m);
 Console.WriteLine($"Чисел больше 0 -> {BigNumNol(numbers)} ");
